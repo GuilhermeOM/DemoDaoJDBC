bc2e45406530bd5a0b8b50680287bb61c20a3b27
guilherme <guilhermeomalta0@gmail.com>
2021-08-06 20:39:00 -0300

    Interfaces - DepartmentDAO and SellerDAO

--- src/model/dao/DepartmentDAO.java
+++ src/model/dao/DepartmentDAO.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.dao;
 
-/**
- *
- * @author guilherme
- */
+import java.util.List;
+import model.entities.Department;
+
 public interface DepartmentDAO {
     
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
 }
--- src/model/dao/SellerDAO.java
+++ src/model/dao/SellerDAO.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.dao;
 
-/**
- *
- * @author guilherme
- */
+import java.util.List;
+import model.entities.Department;
+import model.entities.Seller;
+
 public interface SellerDAO {
     
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findByDepartment(Department department);
+    List<Seller> findAll();
 }
