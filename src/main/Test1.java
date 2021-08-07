67c35a42dc62c6299dd7c0685527295dc184f248
guilherme <guilhermeomalta0@gmail.com>
2021-08-06 23:10:38 -0300

    SellerDaoJDBC - deleteById

--- src/main/Test1.java
+++ src/main/Test1.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
 import model.entities.Seller;
+import java.util.Scanner;
 
 public class Test1 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
         SellerDAO seDAO = DaoFactory.createSellerDAO();
 //==============================================================================
@@ -16,5 +18,11 @@ public class Test1 {
 //==============================================================================
 //        Seller sellerTest = new Seller(10, "Tester2.0", "test2@gmail.com", new Date(), 12345.00, new Department(2, null));
 //        seDAO.update(sellerTest);
+//        System.out.println("Updated!");
+//==============================================================================
+//        System.out.print("Enter the ID to delete: ");
+//        int id = scan.nextInt();
+//        seDAO.deleteById(id);
+//        System.out.println("Deleted!");
     }
 }
